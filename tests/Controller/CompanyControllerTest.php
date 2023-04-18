96072187e1536a557220e255e8f672db48a25172
yakob-abada <yakob.abada@gmail.com>
2023-04-18 16:21:59 +0200

    Add controller tests

---
+++ tests/Controller/CompanyControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CompanyControllerTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $client = static::createClient();
+        $client->followRedirects(true);
+        $client->request('GET', '/company');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', 'New Company');
+    }
+}
