afc8f69c27be53093431a77f5da9b6b19ae1d11a
yakob-abada <yakob.abada@gmail.com>
2023-04-19 08:59:32 +0200

    Code refactoring.

--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -4,18 +4,15 @@ namespace App\Controller;
 
 use App\Model\Company;
 use App\Form\Type\CompanyType;
-use App\Service\HistoricalQuotes;
 use App\Service\NewCompanySubmissionMediator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-#[Route('/company')]
 class CompanyController extends AbstractController
 {
-    #[Route('/', name: 'create_company')]
+    #[Route('/', name: 'company_index')]
     public function index(Request $request, NewCompanySubmissionMediator $submissionMediator): Response
     {
         $company = new Company();
@@ -24,7 +21,6 @@ class CompanyController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // will do something here.
             $historicalQuotes = $submissionMediator->mediate($company);
 
             return $this->render('company/get.html.twig', [
@@ -34,7 +30,6 @@ class CompanyController extends AbstractController
         }
 
         return $this->render('company/index.html.twig', [
-            'controller_name' => 'CompanyController',
             'form' => $form,
         ]);
     }
